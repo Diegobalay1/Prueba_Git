365846b458f5f651237f1bbff60dcab4e6a7ec56
Diegobalay1 <dlolhd@gmail.com>
2016-07-30 21:42:03 +0200

    Commit inicial del proyecto

---
+++ app/src/main/java/es/diego3l/prueba_git/MainActivity.java
@@ -0,0 +1,13 @@
+package es.diego3l.prueba_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
