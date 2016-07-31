fcb41469d3e12d1e35316a97f5a226808ffa079d
Diegobalay1 <dlolhd@gmail.com>
2016-07-31 15:00:10 +0200

    Aplicamos lógica y aplicamos una condición para cambiar el texto cuando presionamos el botón.

--- app/src/main/java/es/diego3l/prueba_git/MainActivity.java
+++ app/src/main/java/es/diego3l/prueba_git/MainActivity.java
@@ -2,12 +2,34 @@ package es.diego3l.prueba_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button boton;
+    TextView texto;
+    boolean condicion = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texto = (TextView) findViewById(R.id.text);
+        boton = (Button) findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!condicion){
+                   texto.setText("Cambiamos Texto");
+                    condicion = true;
+                } else {
+                    texto.setText("Hola Mundo");
+                    condicion = false;
+                }
+            }
+        });
+
     }
 }
